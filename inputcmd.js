546cf7f79101f029a4a9759169061be0237b0633
Dhiraj Y <dhiraj.y@vserv.com>
2022-04-21 10:41:49 +0530

    adding input cmd functionality

--- inputcmd.js
+++ inputcmd.js
@@ -3,7 +3,8 @@ and create and remove file on basic of command input
  */
 const fs = require('fs');
 const input = process.argv; // take input from cmd
-
+//commnand add example ->node test.js add test.txt 'this is test'
+//commnand remove example ->node test.js remove 
 if(input[2]=='add'){
     fs.writeFileSync(input[3],input[4]);
 }else if(input[2]=='remove'){
